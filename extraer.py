8ce31e0d0317a1d4a42c1c40d2a790e0ceaec9d5
LukaSaldivia <saldivialuka@gmail.com>
2025-01-13 08:04:35 -0300

    beatiful a bit

--- extraer.py
+++ extraer.py
@@ -1,8 +1,10 @@
 import re
 import json
+from datetime import datetime
 
 # Ruta del archivo de entrada y salida
 input_file = 'may.rtf'
+changes_file = 'cambios.rtf'
 output_file_json = 'output.json'
 
 data = {}
@@ -32,19 +34,45 @@ with open(input_file, 'r') as infile:
        
         for match in matches:
 
-            if match.groups()[0] == '1860': # Si es un grupo
+            posx = match.groups()[0]
+
+            if posx == '1860': # Si es un grupo
                 current_group = match.groups()[1].strip()
                 data['grupos'][current_group] = {}
 
-            if match.groups()[0] == '7980': # Si es un precio
+            if posx == '7980': # Si es un precio
                 precio = match.groups()[1].split(' ')[-1]
 
-            if match.groups()[0] == '4620': # Si es un producto
+            if posx == '4620': # Si es un producto
                 current_product = match.groups()[1].strip()
                 data['grupos'][current_group][current_product] = precio
                 precio = None
 
+with open(changes_file, 'r') as infile:
+    for line in infile:
+        # POS X 1860 -> grupo de venta ej : \posx1860\pvpg\posy9060 Quesada, -> \posx1860\pvpg\posyN Nombre
+        # POS X 4620 -> producto ej: \posx4620\pvpg\posy9540 BARRA SABORIZADA QUESADA -> \posx4620\pvpg\posyN Nombre
+        # POS X 7980 -> precio de producto ej : \posx7980\pvpg\posy3615\qr\absw1320 5205.720, -> \posx7980\pvpg\posyN\qr\absw1320 $precio
+        print(line)
+       
+        matches = re.finditer(r'\\pard \\plain \\nowrap\\f0\\fs18\\phpg\\posx(1860|4620|7980)\\pvpg\\posy[0-9]+(.*?)\s?\\par', line)  # Busca todas las coincidencias
+       
+        for match in matches:
 
+
+            posx = match.groups()[0]
+
+            # if posx == '1860': # Si es un grupo
+            #     current_group = match.groups()[1].strip()
+            #     data['grupos'][current_group] = {}
+
+            # if posx == '7980': # Si es un precio
+            #     precio = match.groups()[1].split(' ')[-1]
+
+            # if posx == '4620': # Si es un producto
+            #     current_product = match.groups()[1].strip()
+            #     data['grupos'][current_group][current_product] = precio
+            #     precio = None
 
 
 
